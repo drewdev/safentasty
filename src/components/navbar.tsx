e8dfa97bcb4629e4600e7a3309ba0719c7321665
drewdev <pedro.c.maureira@gmail.com>
2024-09-14 17:30:50 -0300

    feat: updated navbar color

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,21 +4,21 @@ import { motion } from 'framer-motion';
 const Navbar = () => {
   return (
     <>
-      <nav className="bg-white shadow-md fixed top-0 left-0 w-full z-50">
+      <nav className="bg-teal-500 shadow-md fixed top-0 left-0 w-full z-50">
         <div className="container mx-auto px-4 py-4 flex flex-col md:flex-row justify-between items-center">
           <motion.div whileHover={{ scale: 1.1 }} className="text-2xl font-semibold">
-            <Link to="/" className="text-teal-400 hover:text-teal-600">Safe-n-Tasty</Link>
+            <Link to="/" className="text-teal-100 hover:text-white">Safe-n-Tasty</Link>
           </motion.div>
 
-          <ul className="flex space-x-8 text-xs md:text-sm custom-xs:text-xxs">
+          <ul className="flex space-x-8 text-xs md:text-sm custom-xs:text-xxs mt-2">
             <motion.li whileHover={{ scale: 1.1 }}>
-              <Link to="#" className="text-gray-700 hover:text-green-600 transition duration-300">Become a seller</Link>
+              <Link to="#" className="text-gray-700 hover:text-white transition duration-300">Become a seller</Link>
             </motion.li>
             <motion.li whileHover={{ scale: 1.1 }}>
-              <Link to="#" className="text-gray-700 hover:text-green-600 transition duration-300">About Us</Link>
+              <Link to="#" className="text-gray-700 hover:text-white transition duration-300">About Us</Link>
             </motion.li>
             <motion.li whileHover={{ scale: 1.1 }}>
-              <Link to="#" className="text-gray-700 hover:text-green-600 transition duration-300">Contact</Link>
+              <Link to="#" className="text-gray-700 hover:text-white transition duration-300">Contact</Link>
             </motion.li>
           </ul>
         </div>
